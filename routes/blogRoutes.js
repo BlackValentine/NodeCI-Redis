c3c5d5d79962931f7235f56b0c5f0eced6b94bce
Stephen Dang <son.dang@frontfundr.com>
2024-07-02 22:25:09 +0700

    Stephen code

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const cleanCache = require('../middlewares/cleanCache')
 
 const Blog = mongoose.model('Blog');
 
@@ -13,9 +14,10 @@ module.exports = app => {
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
-
+  app.get('/api/blogs', requireLogin, cleanCache, async (req, res) => {
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key: req.user.id
+    });
     res.send(blogs);
   });
 
@@ -28,11 +30,13 @@ module.exports = app => {
       _user: req.user.id
     });
 
+
     try {
       await blog.save();
       res.send(blog);
     } catch (err) {
       res.send(400, err);
     }
+    clearHash(req.user.id)
   });
 };
